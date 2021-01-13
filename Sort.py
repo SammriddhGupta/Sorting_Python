73c77235ac7212922b9c374fe981e6b17641eb15
Sammriddh Gupta <72302725+CoderSam007@users.noreply.github.com>
2021-01-13 22:28:27 +0530

    Add files via upload

---
+++ Sort.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+ #program to use bubble sort in python 
+ 
+def bubbleSort(arr): 
+    n = len(arr) 
+    for i in range(n-1): 
+        for j in range(0, n-i-1): 
+            if arr[j] > arr[j+1]: 
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+A=[23,45,64,21,98,34,56]
+print(bubbleSort(A))
